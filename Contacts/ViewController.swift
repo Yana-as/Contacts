bdd877e60ea1bfc016eb63544694155f824d5e42
Yana Sychevskaya <sychevskaya404@gmail.com>
2020-07-16 17:58:44 +0300

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Yana on 16.07.2020.
+//  Copyright © 2020 YanaInc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
